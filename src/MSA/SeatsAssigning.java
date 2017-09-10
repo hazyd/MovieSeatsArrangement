7ef0017af029c49424f57b397660acd574f557a5
Wenlin <hazyd@WenlindeMacBook-Pro.local>
2017-09-10 13:12:44 -0400

    Initial Greedy implementation for seats assigning

---
+++ src/MSA/SeatsAssigning.java
@@ -0,0 +1,13 @@
+/**
+ * Created by hazyd on 9/9/17.
+ */
+public class SeatsAssigning {
+    int[][] seats;
+    int row;
+    int col;
+    public SeatsAssigning(int r, int c) {
+        this.row = r;
+        this.col = c;
+        seats = new int[r][c];
+    }
+}
---
+++ src/MSA/Solution.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hazyd on 9/9/17.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("!!!");
+    }
+}
