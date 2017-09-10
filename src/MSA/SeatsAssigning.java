ee90f4be101092a730d8ba387d6a2b7ee8a476b4
Wenlin <hazyd@WenlindeMacBook-Pro.local>
2017-09-10 18:10:19 -0400

    Add addSeats features

--- src/MSA/SeatsAssigning.java
+++ src/MSA/SeatsAssigning.java
@@ -1,13 +1,69 @@
+package MSA;
+
 /**
  * Created by hazyd on 9/9/17.
  */
+import java.util.List;
+import java.util.ArrayList;
+
 public class SeatsAssigning {
     int[][] seats;
     int row;
     int col;
+    int startingRow = 0;
+    char[] mapChar = new char[]{'J','I','H','G','F','E','D','C','B','A'};
+
     public SeatsAssigning(int r, int c) {
         this.row = r;
         this.col = c;
         seats = new int[r][c];
+    }
+
+    public List<String> addSeats(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = this.startingRow; i < this.row; i++) {
+            int startingCol = checkStartingCol(i, n);
+            if (startingCol == -1) {
+                continue;
+            } else {
+                for (int j = 0; j < n; j++) {
+                    this.seats[i][startingCol + j] = 1;
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(this.mapChar[i]);
+                    sb.append(startingCol + j + 1);
+                    res.add(sb.toString());
+                }
+                break;
+            }
+        }
+        for (int i = 0; i < res.size(); i++) {
+            System.out.println(res.get(i));
+        }
+        return res;
+    }
+
+    public void printSeats() {
+        for (int i = 0; i < this.row; i++) {
+            for (int j = 0; j < this.col; j++) {
+                System.out.print(this.seats[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public int checkStartingCol(int rowNum, int n) {
+        int count = 0;
+        for (int i = this.col - 1; i >= 0; i--) {
+            if (this.seats[rowNum][i] == 1) {
+                break;
+            } else {
+                count++;
+            }
+        }
+        if (count + 1 < n) {
+            return -1;
+        } else {
+            return this.col == count ? 0 : this.col - (count - 1);
+        }
     }
 }
--- src/MSA/Solution.java
+++ src/MSA/Solution.java
@@ -1,8 +1,15 @@
+package MSA;
+
 /**
  * Created by hazyd on 9/9/17.
  */
+
+import MSA.SeatsAssigning;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("!!!");
+        SeatsAssigning sa = new SeatsAssigning(10, 20);
+        sa.addSeats(21);
+        sa.printSeats();
     }
 }
